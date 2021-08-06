094dc0cf53bfff6b9cafc2b11c46e52c9dc2b151
Alberto Chamorro <achamorro@vivelibre.es>
2021-08-06 07:49:42 +0200

    :bug: Solved problems with daily rate limit of requests of PokeApi

---
+++ src/components/CategoryCard.jsx
@@ -0,0 +1,19 @@
+export default function CategoryCard({
+  href,
+  title,
+  color,
+  image,
+  pill,
+}) {
+  return (
+    <a
+      className={`card ${color} ${pill ? 'pill' : ''}`}
+      href={href}
+    >
+      <h2>{ title }</h2>
+      {
+        image && <img src={image} alt={title} />
+      }
+    </a>
+  )
+}
---
+++ src/components/CharacterCard.vue
@@ -0,0 +1,56 @@
+<template>
+  <article v-if="pokemon" class="character-card">
+      <section>
+        <h2 class="character-title">{{pokemon.name}}</h2>
+        <div class="character-detail">
+          
+          <h3>height</h3>
+          <span>{{pokemon.height}}</span>
+          
+          <h3>weight</h3>
+          <span>{{pokemon.weight}}</span>
+
+          <h3>stats</h3>
+          <ul>
+            <li
+              v-for="stat in stats"
+              :key="stat"
+            >
+              {{ stat[0] }} : {{ stat[1] }}</li>
+          </ul>
+          
+          <h3>abilities</h3>
+          <ul>
+            <li
+              v-for="ability in abilities"
+              :key="ability"
+            >
+              {{ ability }}
+            </li>
+          </ul>
+          
+        </div>
+      </section>
+      <div :class="`character-image-container ${pokemon.color}-before`">
+        <img class="character-image-container__image" :src="pokemon.image" :alt="pokemon.name" />
+      </div>
+    </article>
+
+    <h2 v-else>Pokémon not found</h2>
+</template>
+
+<script>
+export default {
+  props: {
+    pokemon: Object,
+  },
+  computed: {
+    stats() {
+      return Object.entries(this.pokemon.stats);
+    },
+    abilities() {
+      return Object.values(this.pokemon.abilities);
+    },
+  },
+}
+</script>
